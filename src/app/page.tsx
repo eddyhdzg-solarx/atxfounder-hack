9d190ff05481624cdf78c8c254e0951142b83084
Eddy Hernández <eddy@solarx.app>
2025-03-01 05:34:43 -0600

    :rocket: cloudflare

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,16 @@
 import { ScrapedTable } from "@/components/ScrapedTable";
 import { StepList } from "@/components/StepList";
+import { getDemo } from "@/demo/getRecipe";
 import { getEvents } from "@/server";
 import { getFindEvents } from "@/utils/getFindEvents";
+import { getCloudflareContext } from "@opennextjs/cloudflare";
 
 export default async function Home() {
   const { marisaEvents, entreEvents, mergedEvents, findEvents } =
     await getFindEvents();
   const events = await getEvents();
+  const { env } = getCloudflareContext();
+  const recipe = await getDemo(env);
 
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
@@ -19,6 +23,7 @@ export default async function Home() {
           dbEvents={events.length}
         />
         <ScrapedTable events={events} />
+        <pre>{JSON.stringify(recipe, null, 2)}</pre>
       </main>
     </div>
   );
